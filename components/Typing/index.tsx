1a03ca425e3b8278bf91343d2d7f873b9160a961
lixiaolong.xlsama <lixiaolong.xlsama@bytedance.com>
2022-06-28 18:15:00 +0800

    feat: init

---
+++ components/Typing/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import css from './index.module.scss'
+
+type Props = {}
+
+const Typing: NextPage<Props> = () => {
+  return <h1>typing</h1>
+}
+
+export default Typing
